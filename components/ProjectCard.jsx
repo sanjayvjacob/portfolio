84ef4f4d904298ac726bc40a5bfb0fdf18fd93ae
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-06-06 12:46:28 +0100

    adjusted poject card styling

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -9,7 +9,7 @@ const ProjectCard = ({project}) => {
     <Card className='group overflow-hidden relative w-full sm:w-[392px] sm:h-[494px]' >
       <CardHeader className='p-0'>
         {/* image */}
-        <div className="relative w-full h-[300px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark
+        <div className="relative w-full h-[290px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark
         xl:bg-[110%] xl:bg-no-repeat overflow-hidden">
           <Image
           className="absolute button-0 shadow-2xl"
@@ -39,7 +39,7 @@ const ProjectCard = ({project}) => {
       <div className="h-full px-8 py-6">
         <Badge className='uppercase text-sm font-medium mb-2 absolute top-4 left-5'>{project.category}</Badge>
         <h4 className="h4 mb-1">{project.name}</h4>
-        <p className="text-muted-foreground text-lg">{project.description}</p>
+        <p className="text-muted-foreground text-lg text-justify">{project.description}</p>
       </div>
     </Card>
   )
