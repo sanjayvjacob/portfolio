c65417ac7db24647f90966a5869eea822bbb7166
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-06-06 17:18:57 +0100

    mobileNav modified

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { AlignJustify } from 'lucide-react'
 import { usePathname } from "next/navigation"
+import Socials from './Socials'
 
 const links = [
   {
@@ -22,25 +23,33 @@ const MobileNav = () => {
   const pathname = usePathname()
   return (
     <Sheet>
-      <SheetTrigger asChild>
+      <SheetTrigger className="flex justify-center items-center">
         <AlignJustify className='cursor-pointer text-primary'/>
       </SheetTrigger>
-      <SheetContent className='flex flex-col'>
-      <div className="mt-32 mb-40 text-center text-2xl"><Link href="/">
+      <SheetContent className='flex flex-col justify-between h-full'>
+        <div>
+      <div className="mt-32 mb-40 text-center text-2xl">
+        <Link href="/">
           <h1 className="text-4xl font-semibold">
-            SanjayJacob<span className="text-primary">.</span></h1></Link></div>
+            SanjayJacob<span className="text-primary">.</span></h1>
+            </Link>
+            </div>
         <nav className="flex flex-col justify-center items-center gap-8">
           {links.map((link,index)=>{
             return (
             <Link 
             href={link.path} 
             key={index} 
-            className={`${link.path === pathname && "text-primary border-b-2 border-primary"}text-xl capitalize hover:text-primary transition-all`}>{link.name}</Link>
+            className={`text-xl capitalize hover:text-primary transition-all ${
+              link.path === pathname ? "text-primary border-b-2 border-white" : ""
+            }`}>{link.name}</Link>
           )
           })}
         </nav>
-        {/* <Socials 
-        containerStyles='flex gap-x-4' iconStyles='text-2xl'/> */}
+        </div>
+        <div className="mb-8">
+          <Socials containerStyles='flex gap-x-4 justify-center' iconStyles='text-2xl'/>
+        </div>
       </SheetContent>
     </Sheet>
   )
